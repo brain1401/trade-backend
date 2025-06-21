0e784a67db64fe72939e497ce512949ada804701
HTG <brain1401@gmail.com>
2025-06-21 13:32:26 +0900

    docs: HsCodeRadarApplication 클래스에 주석 추가 - 애플리케이션 기능 설명 및 메인 메서드에 대한 설명 포함

--- src/main/java/com/hscoderadar/HsCodeRadarApplication.java
+++ src/main/java/com/hscoderadar/HsCodeRadarApplication.java
@@ -6,14 +6,32 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+/**
+ * HS Code Radar 애플리케이션의 메인 클래스
+ * 
+ * <p>
+ * 이 클래스는 Spring Boot 애플리케이션의 시작점이며, 다음 기능들을 활성화합니다:
+ * <ul>
+ * <li>JPA Auditing - 엔티티의 생성/수정 시간 자동 추적</li>
+ * <li>스케줄링 - 주기적인 작업 실행</li>
+ * <li>트랜잭션 관리 - 데이터베이스 트랜잭션 처리</li>
+ * </ul>
+ * 
+ * @author Development Team
+ * @since 1.0.0
+ */
 @SpringBootApplication
 @EnableJpaAuditing
 @EnableScheduling
 @EnableTransactionManagement
 public class HsCodeRadarApplication {
 
+  /**
+   * 애플리케이션 시작점
+   * 
+   * @param args 명령행 인수
+   */
   public static void main(String[] args) {
     SpringApplication.run(HsCodeRadarApplication.class, args);
   }
-
 }
