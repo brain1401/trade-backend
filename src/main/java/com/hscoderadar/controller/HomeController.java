ae0633e02c2dcc047ab208a7589ccadc11bfaaab
KKB <godsb1025@gmail.com>
2025-06-22 23:27:53 +0900

    docs: import 정리

--- src/main/java/com/hscoderadar/controller/HomeController.java
+++ src/main/java/com/hscoderadar/controller/HomeController.java
@@ -2,8 +2,6 @@ package com.hscoderadar.controller;
 
 import com.hscoderadar.common.response.ApiResponseMessage;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 
 /**
