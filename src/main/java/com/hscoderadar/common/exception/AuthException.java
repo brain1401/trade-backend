32039e99db9f08dde57b77155b13d80ac987f286
Aiden <brain1401@gmail.com>
2025-06-23 02:53:28 +0900

    feat: 인증 관련 비즈니스 예외 클래스 AuthException 추가

---
+++ src/main/java/com/hscoderadar/common/exception/AuthException.java
@@ -0,0 +1,52 @@
+package com.hscoderadar.common.exception;
+
+import lombok.Getter;
+
+/**
+ * 인증 관련 비즈니스 예외 클래스
+ * 
+ * 사용자 열거 공격 방지를 위해 모든 인증 실패는 AUTH_001로 통일하여 처리
+ * 
+ * @author HsCodeRadar Team
+ * @since 2.4.0
+ */
+@Getter
+public class AuthException extends RuntimeException {
+
+  private final ErrorCode errorCode;
+
+  public AuthException(ErrorCode errorCode) {
+    super(errorCode.getMessage());
+    this.errorCode = errorCode;
+  }
+
+  public AuthException(ErrorCode errorCode, Throwable cause) {
+    super(errorCode.getMessage(), cause);
+    this.errorCode = errorCode;
+  }
+
+  // 사용자 열거 공격 방지를 위한 통합 인증 실패 예외
+  public static AuthException invalidCredentials() {
+    return new AuthException(ErrorCode.AUTH_001);
+  }
+
+  // 계정 잠김
+  public static AuthException accountLocked() {
+    return new AuthException(ErrorCode.AUTH_002);
+  }
+
+  // 토큰 만료
+  public static AuthException tokenExpired() {
+    return new AuthException(ErrorCode.AUTH_003);
+  }
+
+  // 인증 정보 오류
+  public static AuthException invalidToken() {
+    return new AuthException(ErrorCode.AUTH_004);
+  }
+
+  // 접근 권한 없음
+  public static AuthException accessDenied() {
+    return new AuthException(ErrorCode.AUTH_005);
+  }
+}
