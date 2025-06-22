5cde3699631e2a8e3936cc950aff245f5eb27030
KKB <godsb1025@gmail.com>
2025-06-22 23:46:16 +0900

    feat: search 컨트롤러 dto 추가

---
+++ src/main/java/com/hscoderadar/domain/search/dto/AnalyzeRequest.java
@@ -0,0 +1,10 @@
+package com.hscoderadar.domain.search.dto;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class AnalyzeRequest {
+    private String query;
+}
