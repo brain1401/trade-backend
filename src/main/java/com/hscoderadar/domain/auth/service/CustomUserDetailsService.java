cfae021a2efa1fce372deb690ac174c25865a4b2
Aiden <brain1401@gmail.com>
2025-06-27 23:28:51 +0900

    refactor: CustomUserDetailsService 코드 정리 - 들여쓰기 및 가독성 향상

--- src/main/java/com/hscoderadar/domain/auth/service/CustomUserDetailsService.java
+++ src/main/java/com/hscoderadar/domain/auth/service/CustomUserDetailsService.java
@@ -13,13 +13,13 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class CustomUserDetailsService implements UserDetailsService {
 
-    private final UserRepository userRepository;
+  private final UserRepository userRepository;
 
-    @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        User user = userRepository.findByEmail(username)
-                .orElseThrow(() -> new UsernameNotFoundException("해당하는 유저를 찾을 수 없습니다."));
+  @Override
+  public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+    User user = userRepository.findByEmail(username)
+        .orElseThrow(() -> new UsernameNotFoundException("해당하는 유저를 찾을 수 없습니다."));
 
-        return new PrincipalDetails(user);
-    }
+    return new PrincipalDetails(user);
+  }
 }
