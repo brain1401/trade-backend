ae97e4fae4708fcf08cdd8f76dcd7c902ebbce8d
Aiden <brain1401@gmail.com>
2025-06-30 12:31:45 +0900

    refactor: NotificationSettingsDto 클래스의 Response 및 UpdateRequest를 레코드로 변경하여 코드 간결화

--- src/main/java/com/hscoderadar/domain/notification/dto/NotificationSettingsDto.java
+++ src/main/java/com/hscoderadar/domain/notification/dto/NotificationSettingsDto.java
@@ -2,35 +2,31 @@ package com.hscoderadar.domain.notification.dto;
 
 import com.hscoderadar.domain.user.entity.UserSettings;
 import lombok.Builder;
-import lombok.Getter;
 
 import java.time.LocalTime;
 
 public class NotificationSettingsDto {
 
-    @Getter
-    @Builder
-    public static class Response {
-        private final boolean smsNotificationEnabled;
-        private final boolean emailNotificationEnabled;
-        private final String notificationFrequency;
-        private final LocalTime notificationTime;
-
-        public static Response from(UserSettings settings) {
-            return Response.builder()
-                    .smsNotificationEnabled(settings.isSmsNotificationEnabled())
-                    .emailNotificationEnabled(settings.isEmailNotificationEnabled())
-                    .notificationFrequency(settings.getNotificationFrequency())
-                    .notificationTime(settings.getNotificationTime())
-                    .build();
-        }
+  @Builder
+  public record Response(
+      boolean smsNotificationEnabled,
+      boolean emailNotificationEnabled,
+      String notificationFrequency,
+      LocalTime notificationTime) {
+    public static Response from(UserSettings settings) {
+      return Response.builder()
+          .smsNotificationEnabled(settings.isSmsNotificationEnabled())
+          .emailNotificationEnabled(settings.isEmailNotificationEnabled())
+          .notificationFrequency(settings.getNotificationFrequency())
+          .notificationTime(settings.getNotificationTime())
+          .build();
     }
+  }
 
-    @Getter
-    public static class UpdateRequest {
-        private boolean smsNotificationEnabled;
-        private boolean emailNotificationEnabled;
-        private String notificationFrequency;
-        private LocalTime notificationTime;
-    }
+  public record UpdateRequest(
+      boolean smsNotificationEnabled,
+      boolean emailNotificationEnabled,
+      String notificationFrequency,
+      LocalTime notificationTime) {
+  }
 }
