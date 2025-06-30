35cc4e516b5276cc3f66454fe72ebf8fb211efb8
Aiden <brain1401@gmail.com>
2025-07-01 03:44:42 +0900

    feat: JAXB에서 Jackson으로 XML 매핑 방식 변경 및 관련 애노테이션 수정

--- src/main/java/com/hscoderadar/domain/exchange/dto/response/CustomsExchangeRateResponse.java
+++ src/main/java/com/hscoderadar/domain/exchange/dto/response/CustomsExchangeRateResponse.java
@@ -1,29 +1,26 @@
 package com.hscoderadar.domain.exchange.dto.response;
 
-import jakarta.xml.bind.annotation.XmlAccessType;
-import jakarta.xml.bind.annotation.XmlAccessorType;
-import jakarta.xml.bind.annotation.XmlElement;
-import jakarta.xml.bind.annotation.XmlRootElement;
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlElementWrapper;
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty;
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlRootElement;
 import java.util.List;
 
 /**
- * 관세청 환율 정보 API의 전체 XML 응답을 매핑하는 최상위 DTO. JAXB 어노테이션을 사용하여 XML 구조와 정확히 매핑함.
+ * 관세청 환율 정보 API의 전체 XML 응답을 매핑하는 최상위 DTO. Jackson 애노테이션을 사용하여 XML 구조와 정확히 매핑함.
  */
-@XmlRootElement(name = "trifFxrtInfoQryRtnVo")
-@XmlAccessorType(XmlAccessType.FIELD)
+@JacksonXmlRootElement(localName = "trifFxrtInfoQryRtnVo")
 public record CustomsExchangeRateResponse(
-    @XmlElement(name = "tCnt") int totalCount,
-    @XmlElement(name = "trifFxrtInfoQryRsltVo") List<Item> itemList) {
+    @JacksonXmlProperty(localName = "tCnt") int totalCount,
+    @JacksonXmlElementWrapper(useWrapping = false) @JacksonXmlProperty(localName = "trifFxrtInfoQryRsltVo") List<Item> itemList) {
 
   /** 개별 환율 정보를 담는 DTO. */
-  @XmlAccessorType(XmlAccessType.FIELD)
   public record Item(
-      @XmlElement(name = "currSgn") String currencyCode, // 통화부호
-      @XmlElement(name = "mtryUtNm") String currencyName, // 화폐단위명
-      @XmlElement(name = "fxrt") String exchangeRate, // 환율
-      @XmlElement(name = "aplyBgnDt") String notifiedDate, // 적용개시일자
-      @XmlElement(name = "imexTp") String rateType, // 수출입구분 (1:수출, 2:수입)
-      @XmlElement(name = "cntySgn") String countrySign // 국가부호
+      @JacksonXmlProperty(localName = "currSgn") String currencyCode, // 통화부호
+      @JacksonXmlProperty(localName = "mtryUtNm") String currencyName, // 화폐단위명
+      @JacksonXmlProperty(localName = "fxrt") String exchangeRate, // 환율
+      @JacksonXmlProperty(localName = "aplyBgnDt") String notifiedDate, // 적용개시일자
+      @JacksonXmlProperty(localName = "imexTp") String rateType, // 수출입구분 (1:수출, 2:수입)
+      @JacksonXmlProperty(localName = "cntySgn") String countrySign // 국가부호
   ) {
   }
 }
