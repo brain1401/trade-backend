165f183f5550d0e4a5d679401c9c4bc18ca725fb
Aiden <brain1401@gmail.com>
2025-06-30 19:50:28 +0900

    feat: ExchangeRateController에서 ExchangeRateDto를 ExchangeRateResponse로 변경하고 API 경로를 수정함

--- src/main/java/com/hscoderadar/domain/exchange/controller/ExchangeRateController.java
+++ src/main/java/com/hscoderadar/domain/exchange/controller/ExchangeRateController.java
@@ -1,7 +1,7 @@
 package com.hscoderadar.domain.exchange.controller;
 
 import com.hscoderadar.common.response.ApiResponseMessage;
-import com.hscoderadar.domain.exchange.dto.ExchangeRateDto;
+import com.hscoderadar.domain.exchange.dto.response.ExchangeRateResponse;
 import com.hscoderadar.domain.exchange.service.ExchangeRateService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,26 +13,27 @@ import reactor.core.publisher.Mono;
 import java.util.List;
 
 @RestController
-@RequestMapping("/exchange-rates")
+@RequestMapping("/api/exchange-rates")
 @RequiredArgsConstructor
 public class ExchangeRateController {
 
-    private final ExchangeRateService exchangeRateService;
+  private final ExchangeRateService exchangeRateService;
 
-    @GetMapping
-    @ApiResponseMessage("환율 정보가 조회되었습니다.")
-    public Mono<List<ExchangeRateDto>> getExchangeRates() {
-        return exchangeRateService.getLatestExchangeRates();
-    }
+  @GetMapping
+  @ApiResponseMessage("환율 정보가 조회되었습니다.")
+  public Mono<List<ExchangeRateResponse>> getExchangeRates() {
+    return exchangeRateService.getLatestExchangeRates();
+  }
 
-    /**
-     * 특정 국가(통화 코드)의 환율 정보 조회 엔드포인트
-     * @param currencyCode 국가를 식별하는 통화 코드 (예: USD, JPY)
-     * @return 특정 국가의 환율 정보 Mono 객체
-     */
-    @GetMapping("/{currencyCode}")
-    @ApiResponseMessage("특정 국가의 환율 정보가 조회되었습니다.")
-    public Mono<List<ExchangeRateDto>> getExchangeRateByCurrency(@PathVariable String currencyCode) {
-        return exchangeRateService.getExchangeRateByCurrency(currencyCode);
-    }
+  /**
+   * 특정 국가(통화 코드)의 환율 정보 조회 엔드포인트
+   * 
+   * @param currencyCode 국가를 식별하는 통화 코드 (예: USD, JPY)
+   * @return 특정 국가의 환율 정보 Mono 객체
+   */
+  @GetMapping("/{currencyCode}")
+  @ApiResponseMessage("특정 국가의 환율 정보가 조회되었습니다.")
+  public Mono<List<ExchangeRateResponse>> getExchangeRateByCurrency(@PathVariable String currencyCode) {
+    return exchangeRateService.getExchangeRateByCurrency(currencyCode);
+  }
 }
